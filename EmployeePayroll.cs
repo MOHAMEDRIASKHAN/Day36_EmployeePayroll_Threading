24ab4dffcae8c74ad807d78c8b88fa0cb4d9ac02
MOHAMEDRIASKHAN <108645671+MOHAMEDRIASKHAN@users.noreply.github.com>
2022-09-19 08:37:01 -0700

    EmployeePayroll Without Thread

---
+++ EmployeePayroll.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll_Threading
+{
+    public class EmployeePayroll
+    {
+        public List<EmployeeDetails> employeePolyeeDetailList = new List<EmployeeDetails>();
+
+        //UC 1
+        public void addEmployeeToPayroll(List<EmployeeDetails> employeePayrollDataList)
+        {
+            employeePayrollDataList.ForEach(employeeData =>
+            {
+                Console.WriteLine("Employee Being Added :" + employeeData.EmployeeName);
+                this.addEmployeeToPayroll(employeeData);
+                Console.WriteLine("Employee added : " + employeeData.EmployeeName);
+            });
+            Console.WriteLine(this.employeePolyeeDetailList.ToString());
+        }
+        public void addEmployeeToPayroll(EmployeeDetails emp)
+        {
+            employeePolyeeDetailList.Add(emp);
+        }
+    }
+}
