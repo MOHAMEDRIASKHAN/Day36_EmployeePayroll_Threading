49c7dbc11247dcd2a7de4fd9663ea0ccc7cd938d
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-09-19 09:02:24 -0700

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace EmployeePayroll_Threading
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the EmployeePayroll using Threading");
+        }
+    }
+}
